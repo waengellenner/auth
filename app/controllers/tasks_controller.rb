aabde655e398e58d6a740e886d67e86be71f3e69
Will Engellenner <will.engellenner@kellogg.northwestern.edu>
2023-05-19 20:27:01 +0000

    cont'd

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,13 @@
 class TasksController < ApplicationController
-  def index
-    @tasks = Task.all
+  def index 
+    @tasks = Task.where({ "user_id" => @current_user["id"] })
     @task = Task.new
   end
 
   def create
     @task = Task.new
     @task["description"] = params["task"]["description"]
+    @task["user_id"] = @current_user["id"]
     @task.save
     redirect_to "/tasks"
   end
